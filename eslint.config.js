48bb06ed39afb1214c173f287d6d5950c00678cf
Frenvius <frenvius@gmail.com>
2025-11-22 19:02:23 -0300

    feat: update ESLint configuration with additional plugins and rules, add EditorConfig and Prettier configuration files

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,74 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import perfectionist from 'eslint-plugin-perfectionist';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 export default tseslint.config(
-  { ignores: ["dist"] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-      "@typescript-eslint/no-unused-vars": "off",
-    },
-  },
+	{ ignores: ['dist', 'jest.config.ts', 'vite.config.ts', 'vite.config.dev.ts'] },
+	{
+		files: ['**/*.{ts,tsx}'],
+		extends: [js.configs.recommended, ...tseslint.configs.recommended],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.browser
+		},
+		plugins: {
+			prettier: prettier,
+			'react-hooks': reactHooks,
+			perfectionist: perfectionist,
+			'react-refresh': reactRefresh,
+			'unused-imports': unusedImports
+		},
+		rules: {
+			...reactHooks.configs.recommended.rules,
+			'prettier/prettier': 'error',
+			'react-hooks/exhaustive-deps': 'off',
+			'unused-imports/no-unused-imports': 'error',
+			'@typescript-eslint/no-explicit-any': 'off',
+			'@typescript-eslint/no-require-imports': 'off',
+			'@typescript-eslint/no-empty-object-type': 'off',
+			'@typescript-eslint/interface-name-prefix': 'off',
+			'@typescript-eslint/no-unsafe-function-type': 'off',
+			'@typescript-eslint/explicit-function-return-type': 'off',
+			'@typescript-eslint/explicit-module-boundary-types': 'off',
+			'perfectionist/sort-maps': ['error', { order: 'asc', type: 'line-length' }],
+			'perfectionist/sort-enums': ['error', { order: 'asc', type: 'line-length' }],
+			'perfectionist/sort-exports': ['error', { order: 'asc', type: 'line-length' }],
+			'perfectionist/sort-objects': ['error', { order: 'asc', type: 'line-length' }],
+			'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+			'perfectionist/sort-jsx-props': ['error', { order: 'asc', type: 'line-length' }],
+			'perfectionist/sort-interfaces': ['error', { order: 'asc', type: 'line-length' }],
+			'perfectionist/sort-union-types': ['error', { order: 'asc', type: 'line-length' }],
+			'perfectionist/sort-object-types': ['error', { order: 'asc', type: 'line-length' }],
+			'perfectionist/sort-named-imports': ['error', { order: 'asc', type: 'line-length' }],
+			'perfectionist/sort-named-exports': ['error', { order: 'asc', type: 'line-length' }],
+			'perfectionist/sort-array-includes': ['error', { order: 'asc', type: 'line-length' }],
+			'unused-imports/no-unused-vars': ['warn',
+				{ vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }
+			],
+			'perfectionist/sort-imports': [
+				'error',
+				{
+					order: 'asc',
+					type: 'line-length',
+					internalPattern: ['^~/'],
+					newlinesBetween: 'always',
+					groups: ['type', ['react', 'builtin', 'external'], ['internal', 'parent', 'sibling', 'index']],
+					customGroups: {
+						type: {
+							react: ['^react$', '^react-.']
+						},
+						value: {
+							lodash: 'lodash',
+							react: ['^react$', '^react-.']
+						}
+					}
+				}
+			]
+		}
+	}
 );
